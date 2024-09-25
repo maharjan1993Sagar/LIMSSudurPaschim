b57d5a1629edeaf8d4f9b01f808661e0c40050d3
Sagar Maharjan <maharjanakancha@gmail.com>
2024-09-25 08:18:06 +0545

    employee label dynamic

--- LIMS.Domain/GeneralCMS/Employee.cs
+++ LIMS.Domain/GeneralCMS/Employee.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace LIMS.Domain.GeneralCMS
 
 {
-   public class Employee:BaseEntity
+    public class Employee : BaseEntity
     {
         public NewsEventFile _image;
 
@@ -20,17 +20,17 @@ namespace LIMS.Domain.GeneralCMS
         public string Designation { get; set; }
         public string Abbrebiation { get; set; }
         public string Address { get; set; }
-        public string  PIS { get; set; }
-        public string  OfficePhone { get; set; }
-        public string  Phone2 { get; set; }
-        public string  Email1 { get; set; }
-        public string  Email2 { get; set; }
-        public string  Extension { get; set; }
-        public string Description{ get; set; }
+        public string PIS { get; set; }
+        public string OfficePhone { get; set; }
+        public string Phone2 { get; set; }
+        public string Email1 { get; set; }
+        public string Email2 { get; set; }
+        public string Extension { get; set; }
+        public string Description { get; set; }
         public string FacebookUrl { get; set; }
         public string TweeterUrl { get; set; }
         public string WebsiteUrl { get; set; }
-        public string UserId {get; set; }
+        public string UserId { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public string UpdatedBy { get; set; }
@@ -39,8 +39,9 @@ namespace LIMS.Domain.GeneralCMS
         public bool IsActive { get; set; }
         public string Status { get; set; }
         public bool IsInformationOfficer { get; set; }
-
         public bool IsOfficeChief { get; set; }
+        public string DesignationLabel { get; set; }
+        public int PositionOfLabel { get; set; }
         public NewsEventFile Image {
             get { return _image ?? (_image = new NewsEventFile()); }
             set { _image = value; }
